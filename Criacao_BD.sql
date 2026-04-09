f610cf03c929de5b1bbd12380fed8a2b893f1d5c
jhiovanni-ribas <rjhiovanni@gmail.com>
2026-04-09 16:39:02 -0300

    Create Criacao_BD.sql

---
+++ Criacao_BD.sql
@@ -0,0 +1,23 @@
+
+CREATE TABLE usuarios (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    e_mail VARCHAR(255) NOT NULL UNIQUE,
+    data_nascimento DATE,
+    endereco VARCHAR(255)
+);
+
+CREATE TABLE destinos (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    descricao VARCHAR(255)
+);
+
+CREATE TABLE reservas (
+    id SERIAL PRIMARY KEY,
+    id_usuario INT,
+    id_destino INT,
+    data DATE,
+    status VARCHAR(50) DEFAULT 'pendente'
+);
+
